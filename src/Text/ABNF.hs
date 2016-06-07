eff230e4dfcc8f38fdbd668700b0d5988ca9e74e
Xandaros <mz.bremerhaven@gmail.com>
2016-06-07 19:09:28 +0200

    Add Text.ABNF module

---
+++ src/Text/ABNF.hs
@@ -0,0 +1,17 @@
+{-|
+Module      : Text.ABNF
+Description : Top-level module
+Copyright   : (c) Martin Zeller, 2016
+License     : BSD2
+Maintainer  : Martin Zeller <mz.bremerhaven@gmail.com>
+Stability   : experimental
+Portability : non-portable
+-}
+
+module Text.ABNF
+    ( module Text.ABNF.ABNF
+    , module Text.ABNF.Document
+    ) where
+
+import Text.ABNF.ABNF
+import Text.ABNF.Document
